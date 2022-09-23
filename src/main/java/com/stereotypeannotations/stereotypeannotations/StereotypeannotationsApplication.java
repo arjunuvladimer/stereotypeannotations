50adc827dd776dfaf98be84d42c31afe99af8016
arjunuvladimer <arjunuvladimer@gmail.com>
2022-09-23 14:32:04 +0530

    intital commit

---
+++ src/main/java/com/stereotypeannotations/stereotypeannotations/StereotypeannotationsApplication.java
@@ -0,0 +1,13 @@
+package com.stereotypeannotations.stereotypeannotations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StereotypeannotationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StereotypeannotationsApplication.class, args);
+	}
+
+}
